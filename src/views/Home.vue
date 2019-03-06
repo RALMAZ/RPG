092cd8c9e6d5be65e28261211a0242e7e4ab9b75
RALMAZ <ralmaz.vue@ya.ru>
2019-03-06 12:19:24 +0200

    Update for test tiles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,7 +25,12 @@ export default {
   name: 'home',
   data() {
     return {
-      user: '',
+      assets: {
+				//images: {
+				//	"grass": "https://i.postimg.cc/JhCQL02M/tiles-ground.png",
+				//	"stone": "https://i.postimg.cc/q7cxMrPn/tiles-ground2.png",
+				//},
+			}
     }
   },
   computed: {
@@ -39,64 +44,64 @@ export default {
   methods: {
     // Init game engine
     initGame() {
-      Crafty.init();
+			Crafty.init();
+			Crafty.load(this.assets, // preload assets
+    		() => {
+					Crafty.sprite(128, "https://i.postimg.cc/YSW3WgT5/tiles-ground.png", {
+    				tilesGround: [0,0]
+					});
 
-	    Crafty.sprite(128, "http://craftyjs.com/demos/isometric/images/sprite.png", {
-	    	grass: [0,0,1,1],
-	    	stone: [1,0,1,1]
-	    });
+					Crafty.sprite(128, "https://i.postimg.cc/ZRzLFnSC/tiles-ground2.png", {
+    				tilesGround2: [0,0]
+					});
 
-	    var iso = Crafty.isometric.size(128);
-	    var z = 0;
-	    for(var i = 20; i >= 0; i--) {
-	    	for(var y = 0; y < 20; y++) {
-	    		var which = Crafty.math.randomInt(0,1);
-	    		var tile = Crafty.e("2D, Canvas, "+ (!which ? "grass" : "stone") +", Mouse")
-	    		.attr('z',i+1 * y+1)
-          .areaMap([64,0],[128,32],[128,96],[64,128],[0,96],[0,32])
-          .bind("Click", function(e) {
-	    			//destroy on right click
-            //right click seems not work in Mac OS
-            //delete it
-            console.log(e.button);
-	    			/*if(e.button === 2)*/ this.destroy();
-	    		}).bind("MouseOver", function() {
-	    			if(this.has("grass")) {
-	    				this.sprite(0,1,1,1);
-	    			} else {
-	    				this.sprite(1,1,1,1);
+					Crafty.sprite(128, "https://i.postimg.cc/cHVBhc87/tiles-three.png", {
+    				three: [0,0]
+					});
+
+	    		var iso = Crafty.isometric.size(72);
+	    		var z = 0;
+	    		for(var i = 20; i >= 0; i--) {
+	    			for(var y = 0; y < 20; y++) {
+							var which = Crafty.math.randomInt(0,1);
+							var zet = i+1 * y+1;
+	    				var tile = Crafty.e("2D, Canvas, "+ (!which ? "tilesGround" : "tilesGround2") +", Mouse")
+	    				.attr('z', zet)
+      		    .bind("Click", (e) => {
+								var three = Crafty.e("2D, Canvas, three, Mouse").attr('z', zet);
+								iso.place(i, y, zet, three);
+      		      //console.log(e.button);
+	    					//this.destroy();
+	    				}).bind("MouseOver", () => {
+
+							}).bind("MouseOut", () => {
+	    					
+	    				});
+
+	    				iso.place(i, y, 0, tile);
 	    			}
-	    		}).bind("MouseOut", function() {
-	    			if(this.has("grass")) {
-	    				this.sprite(0,0,1,1);
-	    			} else {
-	    				this.sprite(1,0,1,1);
-	    			}
+	    		}
+
+	    		Crafty.addEvent(this, Crafty.stage.elem, "mousedown", function(e) {
+	    			if(e.button > 1) return;
+	    			var base = {x: e.clientX, y: e.clientY};
+
+	    			function scroll(e) {
+	    				var dx = base.x - e.clientX,
+	    					dy = base.y - e.clientY;
+	    					base = {x: e.clientX, y: e.clientY};
+	    				Crafty.viewport.x -= dx;
+	    				Crafty.viewport.y -= dy;
+	    			};
+
+	    			Crafty.addEvent(this, Crafty.stage.elem, "mousemove", scroll);
+	    			Crafty.addEvent(this, Crafty.stage.elem, "mouseup", function() {
+	    				Crafty.removeEvent(this, Crafty.stage.elem, "mousemove", scroll);
+	    			});
 	    		});
 
-	    		iso.place(i,y,0, tile);
-	    	}
-	    }
-
-	    Crafty.addEvent(this, Crafty.stage.elem, "mousedown", function(e) {
-	    	if(e.button > 1) return;
-	    	var base = {x: e.clientX, y: e.clientY};
-
-	    	function scroll(e) {
-	    		var dx = base.x - e.clientX,
-	    			dy = base.y - e.clientY;
-	    			base = {x: e.clientX, y: e.clientY};
-	    		Crafty.viewport.x -= dx;
-	    		Crafty.viewport.y -= dy;
-	    	};
-
-	    	Crafty.addEvent(this, Crafty.stage.elem, "mousemove", scroll);
-	    	Crafty.addEvent(this, Crafty.stage.elem, "mouseup", function() {
-	    		Crafty.removeEvent(this, Crafty.stage.elem, "mousemove", scroll);
-	    	});
-	    });
-
-
+				}
+			);
     },
 
   }

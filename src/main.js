e231cdf881ca3269e3a05726faa41ef8b8ed8297
Roman Almazov <ralmaz.vue@ya.ru>
2019-06-12 09:32:42 +0300

    Сonsequently..
    ..Neither the Phaser nor the Pixi.js or Crafty.js is able to provide a normal graphic ap for isometry, with at least primitive physics
    Even building maps works in layers rather than ascending, and movement is reduced to cubes.
    
    I move the development to the Defold Game Engine

--- src/main.js
+++ src/main.js
@@ -1,35 +1,119 @@
-import Vue from 'vue'
-import Phaser, { Game } from 'phaser';
+// import Vue from 'vue'
+// import App from './App.vue'
+// import store from './store'
 
-import Iso from './game';
-import App from './App.vue'
-import store from './store'
+// Vue.config.productionTip = false;
 
-let config = {
-  type: Phaser.AUTO,
-  width: 1280,
-  height: 768,
-  backgroundColor: '#fbf0e4',
-  physics: {
-    default: 'arcade',
-    arcade: {
-        debug: true,
-        debugShowBody: true,
-        debugShowStaticBody: true,
-        debugShowVelocity: true,
-        debugVelocityColor: 0xffff00,
-        debugBodyColor: 0x0000ff,
-        debugStaticBodyColor: 0xffffff
-    }
-  },
-  scene: Iso
+// new Vue({
+//   store,
+//   render: function (h) { return h(App) }
+// }).$mount('#app')
+// 
+var PIXI = require('pixi.js')
+console.log(PIXI)
+// import * as PIXI from 'pixi.js';
+
+import TRAVISO from './game/lib/traviso';
+// import Game from './game';
+
+
+var pixiRoot = new PIXI.Application(800, 600, { backgroundColor : 0x6BACDE });
+document.body.appendChild(pixiRoot.view);
+    
+var instanceConfig =
+{
+    mapDataPath : "./game/mapData.json", // the path to the json file that defines map data, required
+    assetsToLoad : ["./game/assets/assets_map.json", "./game/assets/assets_characters.json"], // array of paths to the assets that are desired to be loaded by traviso, no need to use if assets are already loaded to PIXI cache, default null
+    
+    engineInstanceReadyCallback : onEngineInstanceReady, // callback function that will be called once everything is loaded and engine instance is ready, default null
+    tileSelectCallback : onTileSelect, // callback function that will be called when a tile is selected (call params will be the row and column indexes of the tile selected), default null
+    objectSelectCallback : onObjectSelect, // callback function that will be called when a tile with an interactive map-object on it is selected (call param will be the object selected), default null
+    objectReachedDestinationCallback : onObjectReachedDestination, // callback function that will be called when any moving object reaches its destination (call param will be the moving object itself), default null
+    otherObjectsOnTheNextTileCallback : onOtherObjectsOnTheNextTile // callback function that will be called when any moving object is in move and there are other objects on the next tile (call params will be the moving object and an array of objects on the next tile), default null
 };
 
-new Game(config);
+var engine = TRAVISO.getEngineInstance(instanceConfig, { logEnabled: true });
 
-Vue.config.productionTip = false;
 
-new Vue({
-  store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+// this method will be called when the engine is ready
+function onEngineInstanceReady()
+{
+    pixiRoot.stage.addChild(engine);
+}
+
+// this method will be called everytime a tile is selected in the engine
+function onTileSelect(rowIndex, columnIndex)
+{
+        
+}
+
+// this method will be called when a tile with an interactive map-object on it is selected
+function onObjectSelect(obj)
+{
+    engine.moveCurrentControllableToLocation(obj.mapPos);
+}
+
+// this method will be called when any moving object reaches its destination
+function onObjectReachedDestination(obj)
+{
+    var objectsOnDestination = engine.getObjectsAtLocation(obj.mapPos);
+    for (var i=0; i < objectsOnDestination.length; i++)
+    {
+        // check if there is a flag on the destination tile
+        if(objectsOnDestination[i].type === 10)
+        {
+            obj.changeVisual("flip", false, true, flipAnimFinished);
+            break;
+        }
+    }
+}
+
+// this method will be called when the custom flip anim finished
+function flipAnimFinished (obj)
+{
+    // change the visual of the object so that it will face its last direction
+    obj.changeVisualToDirection(obj.currentDirection, false);
+}
+
+// this method will be called when any moving object is in move and there are other objects on the next tile
+function onOtherObjectsOnTheNextTile(movingObject, objectsOnNewTile)
+{
+    var boxAnim;
+    for (var i=0; i < objectsOnNewTile.length; i++)
+    {
+        // check if there are boxes on the next tile
+        if(objectsOnNewTile[i].type === 12)
+        {
+            boxAnim = createAndStartBoxAnim();
+            engine.addCustomObjectToLocation(boxAnim, objectsOnNewTile[i].mapPos);
+            engine.removeObjectFromLocation(objectsOnNewTile[i]);
+        }
+    }
+}
+
+// creates 6 boxes and spreads them up throuh a simple tween animation
+function createAndStartBoxAnim()
+{
+    // create six seperate boxes to spread
+    var boxAnim = new PIXI.Container();
+    var t = PIXI.Texture.fromFrame("box.png");
+    var box;
+    var boxes = [];
+    for (var i=0; i < 6; i++)
+    {
+        box = new PIXI.extras.AnimatedSprite([t]);
+        box.anchor.x = box.anchor.y = 0.5;
+        boxAnim.addChild(box);
+        boxes[boxes.length] = box;
+    }
+    
+    // play a simple spread anim for the boxes
+    var boxTargets = [[-75, -125], [-50, -100], [-25, -75], [25, -75], [50, -100], [75, -125]];
+    for (var i=0; i < boxes.length; i++)
+    {
+        TweenLite.to( boxes[i].position, 0.5, { x: boxTargets[i][0], y: boxTargets[i][1], ease:"Back.easeOut" } );
+        TweenLite.to( boxes[i], 0.7, { alpha: 0 } );
+    }
+    
+    return boxAnim;
+}

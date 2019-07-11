6404588b7445480f26ad6c579040fcb9a4e490c8
Roman Almazov <ralmaz.vue@ya.ru>
2019-07-11 10:04:21 +0300

    Electron, Vue, Excalibur

---
+++ src/renderer/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+import App from './App'
+import router from './router'
+import store from './store'
+import game from './game'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.game = Vue.prototype.$game = game
+Vue.config.productionTip = false
+
+new Vue({
+  components: { App },
+  router,
+  store,
+  template: '<App/>'
+}).$mount('#app')
